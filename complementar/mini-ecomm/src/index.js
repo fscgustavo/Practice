3e165d627a36fc0d389fe2b723d0597245f001dd
˜Gustavo <gustavo.fonseca@centauro.com.br>
2020-04-07 12:21:10 -0300

    separação em pastas e início de um novo projeto

---
+++ complementar/mini-ecomm/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const rootNode = document.getElementById('root')
+ReactDOM.createRoot(rootNode).render(<App/>);
